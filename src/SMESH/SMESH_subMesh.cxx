51593dbf137e34a8cfe4230e1467beee7dd94b43
eap <eap@opencascade.com>
2006-04-10 15:10:54 +0000

    improve SubMeshesComputed()

--- src/SMESH/SMESH_subMesh.cxx
+++ src/SMESH/SMESH_subMesh.cxx
@@ -197,8 +197,8 @@ bool SMESH_subMesh::SubMeshesComputed()
     // one face with READY_TO_COMPUTE state in order to be able to
     // recompute 3D when a locale triangle hypo changes (see PAL7428).
     // So we check if mesh is really present
-    //bool computeOk = (sm->GetComputeState() == COMPUTE_OK);
-    bool computeOk = ( ds && ( ds->GetNodes()->more() || ds->GetElements()->more() ));
+    bool computeOk = (sm->GetComputeState() == COMPUTE_OK ||
+                      (ds && ( ds->GetNodes()->more() || ds->GetElements()->more() )));
     if (!computeOk)
     {
       const TopoDS_Shape & ss = sm->GetSubShape();
