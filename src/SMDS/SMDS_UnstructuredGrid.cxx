fe3703789b6aae6bdf951e78baa5d3b991d4abe2
prascle <prascle>
2009-10-12 09:13:38 +0000

    PR: test of use of vtk structures in SMDS, first part: only nodes, tested with hexaheddrons

---
+++ src/SMDS/SMDS_UnstructuredGrid.cxx
@@ -0,0 +1,79 @@
+
+
+#include "SMDS_UnstructuredGrid.hxx"
+
+using namespace std;
+
+vtkCellLinks::Link* SMDS_CellLinks::AdjustSize(vtkIdType sz)
+{
+  vtkIdType i;
+  vtkCellLinks::Link *newArray;
+  vtkIdType newSize = sz;
+  vtkCellLinks::Link linkInit = {0,NULL};
+
+  newArray = new vtkCellLinks::Link[newSize];
+
+  for (i=0; i<sz && i<this->Size; i++)
+    {
+    newArray[i] = this->Array[i];
+    }
+
+  for (i=this->Size; i < newSize ; i++)
+    {
+    newArray[i] = linkInit;
+    }
+
+  this->Size = newSize;
+  delete [] this->Array;
+  this->Array = newArray;
+
+  return this->Array;
+}
+
+SMDS_CellLinks* SMDS_CellLinks::New()
+{
+  return new SMDS_CellLinks();
+}
+
+SMDS_CellLinks::SMDS_CellLinks() : vtkCellLinks()
+{
+}
+
+SMDS_CellLinks::~SMDS_CellLinks()
+{
+}
+
+void SMDS_UnstructuredGrid::BuildLinks()
+{
+  // Remove the old links if they are already built
+  if (this->Links)
+    {
+    this->Links->UnRegister(this);
+    }
+
+  this->Links = SMDS_CellLinks::New();
+  this->Links->Allocate(this->GetNumberOfPoints());
+  this->Links->Register(this);
+  this->Links->BuildLinks(this, this->Connectivity);
+  this->Links->Delete();
+}
+
+SMDS_CellLinks* SMDS_UnstructuredGrid::GetCellLinks()
+{
+  return static_cast<SMDS_CellLinks*>(this->Links);
+}
+
+SMDS_UnstructuredGrid* SMDS_UnstructuredGrid::New()
+{
+  return new SMDS_UnstructuredGrid();
+}
+
+SMDS_UnstructuredGrid::SMDS_UnstructuredGrid() : vtkUnstructuredGrid()
+{
+}
+
+SMDS_UnstructuredGrid::~SMDS_UnstructuredGrid()
+{
+}
+
+
---
+++ src/SMDS/SMDS_UnstructuredGrid.hxx
@@ -0,0 +1,41 @@
+/* 
+ * File:   SMDS_UnstructuredGrid.hxx
+ * Author: prascle
+ *
+ * Created on September 16, 2009, 10:28 PM
+ */
+
+#ifndef _SMDS_UNSTRUCTUREDGRID_HXX
+#define	_SMDS_UNSTRUCTUREDGRID_HXX
+
+#include <vtkUnstructuredGrid.h>
+#include <vtkCellLinks.h>
+
+class SMDS_CellLinks: public vtkCellLinks
+{
+public:
+    Link *AdjustSize(vtkIdType sz);
+    
+    //virtual void Delete();
+    static SMDS_CellLinks* New();
+protected:
+    SMDS_CellLinks();
+    ~SMDS_CellLinks();
+};
+
+class SMDS_UnstructuredGrid: public vtkUnstructuredGrid
+{
+public:
+    void BuildLinks(); // initialise un SMDS_CellLinks;
+    SMDS_CellLinks* GetCellLinks();
+
+    //virtual void Delete();
+    static SMDS_UnstructuredGrid* New();
+protected:
+    SMDS_UnstructuredGrid();
+    ~SMDS_UnstructuredGrid();
+};
+
+
+#endif	/* _SMDS_UNSTRUCTUREDGRID_HXX */
+
